aa835ca74fd5d5a0be75ce1277279fe131ce662e
Dark0o <darko_crnic@hotmail.com>
2026-06-30 00:12:19 +0200

    add selecting elements logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,29 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [filterValue, setFilterValue] = useState("none");
+
+  const [selectedElements, setSelectedElements] = useState([]);
+  console.log("Selected elements:", selectedElements);
+
+  const toggleItem = (element) => {
+    console.log("adding", element);
+    const isSelected = selectedElements.some((el) => el.id === element.id);
+
+    if (isSelected) {
+      setSelectedElements(
+        selectedElements.filter((el) => el.id !== element.id),
+      );
+    } else {
+      if (selectedElements.length >= 3) return;
+      setSelectedElements([...selectedElements, element]);
+    }
+  };
+
+  console.log(elements);
   return (
     <>
       <h1>Select items</h1>
+      <p>You currently have {selectedElements.length} items selected.</p>
       <button onClick={() => setIsOpen(!isOpen)}>Change my choice</button>
 
       {isOpen && (
@@ -49,14 +69,32 @@ function App() {
             </label>
           </div>
           <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
-            {elements.map((el) => (
-              <li key={el.id}>
+            {elements.map((element) => (
+              <li key={element.id}>
                 <label>
-                  <input type="checkbox" /> {el.name}
+                  <input
+                    type="checkbox"
+                    onChange={() => toggleItem(element)}
+                    checked={selectedElements.some(
+                      (el) => el.id === element.id,
+                    )}
+                    disabled={
+                      selectedElements.length >= 3 &&
+                      !selectedElements.some((el) => el.id === element.id)
+                    }
+                  />
+                  {element.name}
                 </label>
               </li>
             ))}
           </ul>
+
+          <div>
+            Current selected items:
+            {selectedElements.map((el) => (
+              <span key={el.id}>{el.name}</span>
+            ))}
+          </div>
         </div>
       )}
     </>
