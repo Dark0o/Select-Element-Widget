4f3009687f6f43a57e221e3ce361bbba5dcb47cc
Dark0o <darko_crnic@hotmail.com>
2026-06-29 23:32:58 +0200

    define port and initial markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterValue, setFilterValue] = useState("none");
+  return (
+    <>
+      <h1>Select items</h1>
+      <button onClick={() => setIsOpen(!isOpen)}>Change my choice</button>
+
+      {isOpen && (
+        <div>
+          <h3>Select items</h3>
+          <div style={{ display: "flex", gap: "16px", marginBottom: "8px" }}>
+            <label>
+              Search
+              <input
+                type="text"
+                placeholder="Search..."
+                onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+              />
+            </label>
+            <label>
+              Filter
+              <select
+                value={filterValue}
+                onChange={(e) => setFilterValue(e.target.value)}
+              >
+                <option value="none">No filter</option>
+                <option value="100">&gt;100</option>
+                <option value="2500">&gt;2500</option>
+                <option value="10000">&gt;10000</option>
+              </select>
+            </label>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
